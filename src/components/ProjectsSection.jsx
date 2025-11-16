c7dd1a343c2e443285011c55578ff714b58ed840
William Stucchi <wstucchi53@gmail.com>
2025-11-16 16:09:35 +0100

    Changed padding

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -91,7 +91,7 @@ export const ProjectsSection = () => {
                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                 </p>
 
-                <div className="w-full py-12 bg-background">
+                <div className="w-full py-2 bg-background">
                     <div className="relative">
                         <div 
                         ref={scrollRef}
